1c4c1c0c525563d97fc59e6d5569ac50be93e8f6
phong2k2 <lnbphong.20it2@vku.udn.vn>
2024-10-26 17:19:41 +0700

    Initial commit

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -148,6 +148,7 @@ const getDetailsUser = (id) => {
             const user = await User.findOne({
                 _id: id
             })
+            console.log('user', user)
             if (user === null) {
                 resolve({
                     status: 'OK',
